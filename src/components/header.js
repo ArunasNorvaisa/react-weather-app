354f62c0b6c6f19666c01285d1694890cac3dd21
Arūnas Norvaiša <an@86.lt>
2018-07-29 15:24:18 +0300

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Header = (props) => {
-    console.log(props);
     return <header>
             <div className="coords">Latitude: { props.latitude.toFixed(4) }</div>
             <div className="coords">Longitude: { props.longitude.toFixed(4) }</div>
--- src/components/weather.js
+++ src/components/weather.js
@@ -3,13 +3,16 @@ import ReactWeather from "react-open-weather";
 import "react-open-weather/lib/css/ReactWeather.css";
 
 const Weather = (props) => {
-    return <ReactWeather
-        forecast="5days"  
-        apikey="0dde434f020b43b6adf152044182507"
-        type="geo"
-        lat={ props.latitude.toString() }
-        lon={ props.longitude.toString() }
-    />;
-}
+    return <div>
+        { `${props.latitude}, ${props.longitude}` }
+        <ReactWeather
+            forecast="5days"
+            apikey="0dde434f020b43b6adf152044182507"
+            type="geo"
+            lat={ props.latitude.toString() }
+            lon={ props.longitude.toString() }
+        />
+    </div>;
+};
  
 export default Weather;
