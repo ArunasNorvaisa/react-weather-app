e49a203a0b167a53e866eec09e27b2db9de53762
Arūnas Norvaiša <an@86.lt>
2018-09-28 19:10:25 +0300

    started calculating Sun posX/posY

--- src/components/suncycle.js
+++ src/components/suncycle.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const SunCycle = props => {
     const dayLenght = props.sunset - props.sunrise;
-    const timeNow = (new Date() / 1e3).toFixed(0);
-    const dayProgress = dayLenght - (props.sunset - timeNow);
-    const posX = Math.cos((dayProgress / dayLenght) * (Math.PI / 2));
-    const posY = Math.sin((dayProgress / dayLenght) * (Math.PI / 2));
-    console.log(dayLenght, dayProgress, posX, posY);
+    const dayProgress = props.sunset - (new Date() / 1e3).toFixed(0);
+    const posX = Math.cos((dayProgress / dayLenght) * Math.PI);
+    const posY = Math.sin((dayProgress / dayLenght) * Math.PI);
 
     return <div className="sunCycle">
         <span className="sun"></span>
+        <div>{dayLenght}</div>
+        <div>{dayProgress}</div>
+        <div>{posX}</div>
+        <div>{posY}</div>
     </div>;
 }
 
