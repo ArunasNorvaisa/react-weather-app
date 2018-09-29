1dcde23f1f7bd29ef696958a06e43dbbb60ec78e
Arūnas Norvaiša <an@86.lt>
2018-09-29 14:19:54 +0300

    Sun position calculated

--- src/components/suncycle.js
+++ src/components/suncycle.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 const SunCycle = props => {
-    const dayLenght = props.sunset - props.sunrise;
-    const dayProgress = props.sunset - (new Date() / 1e3).toFixed(0);
-    const posX = Math.cos((dayProgress / dayLenght) * Math.PI);
+    let dayLenght = props.sunset - props.sunrise;
+    let dayProgress = props.sunset - (new Date() / 1e3).toFixed(0);
+    // dayLenght = 1000;
+    // dayProgress = 100;
+    const posX = Math.cos((dayProgress / dayLenght) * Math.PI) + 1;
     const posY = Math.sin((dayProgress / dayLenght) * Math.PI);
 
     return <div className="sunCycle">
-        <span className="sun"></span>
+        <span className="sun">
+            <img
+                src="./images/icons/sun.svg"
+                alt="Sun"
+                style={{
+                    left: `calc(${posX} * 50% - 25px)`,
+                    bottom: `calc((${posY} * 50%) + 50% - 13px)`
+                }}
+            />
+        </span>
         <div>{dayLenght}</div>
         <div>{dayProgress}</div>
         <div>{posX}</div>
