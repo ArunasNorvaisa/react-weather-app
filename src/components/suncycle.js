0c1f4447a2fd47ff64341199c62e795d3a1de5db
Arūnas Norvaiša <an@86.lt>
2018-09-29 14:20:36 +0300

    Sun position calculated

--- src/components/suncycle.js
+++ src/components/suncycle.js
@@ -3,8 +3,6 @@ import React from 'react';
 const SunCycle = props => {
     let dayLenght = props.sunset - props.sunrise;
     let dayProgress = props.sunset - (new Date() / 1e3).toFixed(0);
-    // dayLenght = 1000;
-    // dayProgress = 100;
     const posX = Math.cos((dayProgress / dayLenght) * Math.PI) + 1;
     const posY = Math.sin((dayProgress / dayLenght) * Math.PI);
 
