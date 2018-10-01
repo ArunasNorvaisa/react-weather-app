2d17f92efdc3949f3bd0c2ff8e0e2896f19b75f0
Arūnas Norvaiša <an@86.lt>
2018-10-01 13:24:17 +0300

    Sun cycle fully implemented

--- src/components/suncycle.js
+++ src/components/suncycle.js
@@ -2,12 +2,39 @@ import React from 'react';
 
 const SunCycle = props => {
 
-    let dayLength = props.sunset - props.sunrise;
-    let dayProgress = props.sunset - (new Date() / 1e3).toFixed(0);
-    const sunPositionX = Math.cos((dayProgress / dayLength) * Math.PI) + 1;
-    const sunPositionY = Math.sin((dayProgress / dayLength) * Math.PI);
+    const dayLength = props.sunset - props.sunrise;
+    const timeNow = (new Date() / 1e3).toFixed(0);
+    let dayProgress = props.sunset - timeNow;
+    let sunPositionX = Math.cos((dayProgress / dayLength) * Math.PI) + 1;
+    let sunPositionY = Math.sin((dayProgress / dayLength) * Math.PI);
+
+    if(timeNow < props.sunrise) {
+        sunPositionX = 0;
+        sunPositionY = 0;
+    } else if(timeNow > props.sunset) {
+        sunPositionX = 2;
+        sunPositionY = 0;
+    }
+
+    const getTime = time => {
+        const options = {
+            timeZone: props.timezone,
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: true
+        };
+        return new Date(time * 1e3).toLocaleTimeString('en', options);
+    };
 
     return <div className="sunCycle">
+        <span className="sunrise">
+            <img src="./images/icons/sunrise.svg" alt="Sunrise" />
+            <span className="sunriseTime">{ getTime(props.sunrise) }</span>
+        </span>
+        <span className="sunset">
+            <span className="sunsetTime">{ getTime(props.sunset) }</span>
+            <img src="./images/icons/sunset.svg" alt="Sunset" />
+        </span>
         <span className="sun">
             <img
                 src="./images/icons/sun.svg"
--- src/components/weathernow.js
+++ src/components/weathernow.js
@@ -4,11 +4,19 @@ import SunCycle from './suncycle';
 const WeatherNow = props => {
     const icon_URL = "./images/icons/" + props.JSON.currently.icon + ".svg";
     const timeNow = Math.round(new Date().getTime()/1000);
-    let temperature;
+    let temperatureNow;
+    let temperatureMin;
+    let temperatureMax;
 
-    props.isTemperatureInC ?
-        temperature = props.JSON.currently.temperature.toFixed(0) :
-        temperature = (props.JSON.currently.temperature * 1.8 + 32).toFixed(0);
+    if(props.isTemperatureInC) {
+        temperatureNow = props.JSON.currently.temperature.toFixed(0);
+        temperatureMin = props.JSON.daily.data[0].temperatureLow.toFixed(0);
+        temperatureMax = props.JSON.daily.data[0].temperatureHigh.toFixed(0);
+    } else {
+        temperatureNow = (props.JSON.currently.temperature * 1.8 + 32).toFixed(0);
+        temperatureMin = (props.JSON.daily.data[0].temperatureLow * 1.8 + 32).toFixed(0);
+        temperatureMax = (props.JSON.daily.data[0].temperatureHigh * 1.8 + 32).toFixed(0);
+    }
 
     const getTime = time => {
         const options = {
@@ -30,14 +38,15 @@ const WeatherNow = props => {
                 sunset={ props.JSON.daily.data[0].sunsetTime }
                 timezone={ props.JSON.timezone }
             />
-            <div className="timeNow">{ getTime(timeNow) }</div>
-            <div className="tToday">{ temperature }&deg;{ props.isTemperatureInC ? "C" : "F" }
+            <div className="tToday">{ temperatureMin } / { temperatureMax }&deg;{ props.isTemperatureInC ? "C" : "F" }</div>
+            <div>{ props.JSON.currently.summary }</div>
+            <div className="tNow">{ temperatureNow }&deg;{ props.isTemperatureInC ? "C" : "F" }
                 <img src={ icon_URL } alt={ props.JSON.currently.icon } />
             </div>
-            <div>{ props.JSON.currently.summary }</div>
             <div>
                 { props.JSON.currently.precipType } { (props.JSON.currently.precipProbability * 100).toFixed(0) }%
             </div>
+            <div className="timeNow">{ getTime(timeNow) }</div>
             <hr />
             <div>{ props.JSON.hourly.summary }</div>
         </div> :
@@ -45,12 +54,14 @@ const WeatherNow = props => {
             <SunCycle
                 sunrise={ props.JSON.daily.data[0].sunriseTime }
                 sunset={ props.JSON.daily.data[0].sunsetTime }
+                timezone={ props.JSON.timezone }
             />
-            <div className="timeNow">{ getTime(timeNow) }</div>
-            <div className="tToday">{ temperature }&deg;{ props.isTemperatureInC ? "C" : "F" }
+            <div className="tToday">{ temperatureMin } / { temperatureMax }&deg;{ props.isTemperatureInC ? "C" : "F" }</div>
+            <div>{ props.JSON.currently.summary }</div>
+            <div className="tNow">{ temperatureNow }&deg;{ props.isTemperatureInC ? "C" : "F" }
                 <img src={ icon_URL } alt={ props.JSON.currently.icon } />
             </div>
-            <div>{ props.JSON.currently.summary }</div>
+            <div className="timeNow">{ getTime(timeNow) }</div>
             <hr />
             <div>{ props.JSON.hourly.summary }</div>
         </div>;
