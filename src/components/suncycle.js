8bbdb6a13a946d428d27afaf5461406b40124cd1
Arūnas Norvaiša <an@86.lt>
2018-10-02 10:31:47 +0300

    some code comments

--- src/components/suncycle.js
+++ src/components/suncycle.js
@@ -5,9 +5,12 @@ const SunCycle = props => {
     const dayLength = props.sunset - props.sunrise;
     const timeNow = (new Date() / 1e3).toFixed(0);
     const dayProgress = props.sunset - timeNow;
+    // We're adding 1 to sunpositionX so it be 0÷2 rather than -1÷1
     let sunPositionX = Math.cos((dayProgress / dayLength) * Math.PI) + 1;
     let sunPositionY = Math.sin((dayProgress / dayLength) * Math.PI);
 
+    // 2 conditions valid at night (before sunrise and after sunset), placing the Sun
+    // on the suncycle rather than leaving it somewhere below the semicircle
     if(timeNow < props.sunrise) {
         sunPositionX = 0;
         sunPositionY = 0;
