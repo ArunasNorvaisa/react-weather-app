2adebbf51770a289ead2c165b026c73d023546ab
Arunas <an@86.lt>
2019-06-14 16:22:54 +0300

    app.js updated

--- src/components/app.js
+++ src/components/app.js
@@ -1,81 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Map from './map';
 
-const API_KEY_GOOGLE =`${process.env.REACT_APP_API_KEY_GL}`;
+export default function App() {
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false,
-      latitude: null,
-      longitude: null,
-      city: null
-    };
+  const API_KEY_GOOGLE =`${ process.env.REACT_APP_API_KEY_GL }`;
+
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  const [city, setCity] = useState(null);
+  useEffect(() => {
+    async function getLocation() {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          reverseGeocoded(position.coords.latitude, position.coords.longitude);
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        },
+        err => {
+          console.warn(`ERROR(${err.code}): ${err.message}`);
+          getIP();
+        },
+        {
+          timeout: 6000,
+          enableHighAccuracy: false
+        }
+      );
+    }
+    getLocation();
+
+  });
+
+  async function getIP() {
+    const IP_URL_HOME = 'https://ipapi.co/json/';
+    const response = await fetch(IP_URL_HOME, { method:'GET' });
+    const json = await response.json();
+    setLatitude(json.latitude);
+    setLongitude(json.longitude);
+    setCity(json.city);
+    setIsLoaded(true);
   }
 
-  getIP = () => {
-    const IP_URL_HOME = 'https://ipapi.co/json/';
-    fetch(IP_URL_HOME, { method:'GET' })
-      .then(response => response.json())
-      .then(json => {
-        this.setState({
-          isLoaded: true,
-          latitude: json.latitude,
-          longitude: json.longitude,
-          city: json.city
-        });
-      });
-  };
-
-  reverseGeocoded = (lat, lng) => {
+  const reverseGeocoded = (lat, lng) => {
     let GEO_URL_HOME = `https://maps.googleapis.com/maps/api/geocode/json?key=${API_KEY_GOOGLE}&latlng=`;
-    GEO_URL_HOME += +lat + ',' + lng;
+    GEO_URL_HOME += lat + ',' + lng;
     fetch(GEO_URL_HOME, { method: 'GET' })
       .then(response => response.json())
       .then(json => {
-          this.setState({
-            isLoaded: true,
-            city: json.results[0].address_components[2].short_name
-          });
-        }
-      );
-  };
-
-  getLocation = () => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        this.reverseGeocoded(position.coords.latitude, position.coords.longitude);
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        });
-      },
-      err => {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-        this.getIP();
-      },
-      {
-        timeout: 6000,
-        enableHighAccuracy: false
+        setIsLoaded(true);
+        setCity(json.results[0].address_components[2].short_name);
       }
     );
   };
 
-  componentDidMount() {
-    this.getLocation();
-  };
-
-  render() {
-    const { latitude, longitude, city } = this.state;
-    return this.state.isLoaded ?
-      <React.Fragment>
-        <Map lat={ latitude }
-             lng={ longitude }
-             city={ city }
-             address={ city }
-        />
-      </React.Fragment> :
-      <h2>Application is loading, please be patient...</h2>
-  }
+  return isLoaded ?
+    <React.Fragment>
+      <Map lat={ latitude }
+           lng={ longitude }
+           city={ city }
+           address={ city }
+      />
+    </React.Fragment> :
+    <h2>Application is loading, please be patient...</h2>
 }
