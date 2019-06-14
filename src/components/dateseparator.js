c5f0ff88671d6800b24dd0f5a4a909f1a3925822
Arunas <an@86.lt>
2019-06-14 14:16:29 +0300

    dateseparator.js updated

--- src/components/dateseparator.js
+++ src/components/dateseparator.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import { getTime, getDate } from "../functions/functions";
 
 const DateSeparator = props => {
+
   const getWeekday = unixTime => {
-    let options = {
+    let timeOptions = {
       timeZone: props.timezone,
       hour: '2-digit',
       minute: '2-digit',
       hour12: false
     };
-    const time = new Date(unixTime * 1e3).toLocaleTimeString('en', options);
-    options = {
+    const time = getTime(unixTime, timeOptions);
+    timeOptions = {
       weekday: 'long',
       timeZone: props.timezone
     };
-    const weekday = new Date(unixTime * 1e3).toLocaleDateString('en', options);
-    if(time === "00:00") { return weekday; }
+    const weekday = getDate(unixTime, timeOptions);
+    if(time === "00:00") return weekday;
   };
+
   return getWeekday(props.time) ?
     <div className="weekday">{ getWeekday(props.time) }</div> :
     null;
