ae90df2998efe3e88ee1b936119aa8f07fe4187d
Arunas <an@86.lt>
2019-06-14 13:45:26 +0300

    hourlyweatheritem.js fixed

--- src/components/hourlyweatheritem.js
+++ src/components/hourlyweatheritem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CtoF, getTime } from "../functions/functions";
 
 const HourlyWeatherItem = props => {
   const icon_URL = "./images/icons/" + props.item.icon + ".svg";
@@ -6,32 +7,25 @@ const HourlyWeatherItem = props => {
 
   props.isTemperatureInC ?
     temperature = props.item.temperature.toFixed(0) :
-    temperature = (props.item.temperature * 1.8 + 32).toFixed(0);
+    temperature = CtoF(props.item.temperature).toFixed(0);
 
-  const getTime = time => {
-    const options = {
-      timeZone: props.JSON.timezone,
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: false
-    };
-    return new Date(time * 1e3).toLocaleTimeString('en', options);
+  const timeOptions = {
+    timeZone: props.JSON.timezone,
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
   };
 
-  return props.item.precipType ?
-    <div className="hourlyWeatherItem">
-      <div>{getTime(props.item.time)}</div>
-      <div><img src={icon_URL} alt={props.item.icon}/></div>
-      <div>{temperature}&deg;{props.isTemperatureInC ? "C" : "F"}</div>
+  return <div className="hourlyWeatherItem">
+    <div>{ getTime(props.item.time, timeOptions) }</div>
+    <div><img src={ icon_URL } alt={ props.item.icon } /></div>
+    <div>{ temperature }&deg;{ props.isTemperatureInC ? "C" : "F" }</div>
+    { props.item.precipType &&
       <div>
-        {props.item.precipType} {(props.item.precipProbability * 100).toFixed(0)}%
+        { props.item.precipType } { (props.item.precipProbability * 100).toFixed(0) }%
       </div>
-    </div> :
-    <div className="hourlyWeatherItem">
-      <div>{getTime(props.item.time)}</div>
-      <div><img src={icon_URL} alt={props.item.icon}/></div>
-      <div>{temperature}&deg;{props.isTemperatureInC ? "C" : "F"}</div>
-    </div>;
+    }
+  </div>;
 };
 
 export default HourlyWeatherItem;
