a53b641c7b28f55360b2ad6ebe54dfefb5b9e124
Arunas Norvaisa <an@arunas.org>
2020-11-02 08:23:55 +0200

    Got rid of EventEmitter for the sake of better error handling

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { EventEmitter } from 'events';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Error ({ message }) {
-  const emitter = new EventEmitter();
-  emitter.once('error', () => toast.error(message, {
+
+  const error = new Promise(resolve => toast.error(message, {
     position: 'bottom-right',
     autoClose: false,
     hideProgressBar: true,
     closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
-    className: 'error'
+    className: 'error',
+    onOpen: resolve(true),
+    onClose: resolve(false)
   }));
 
-  emitter.emit('error');
+  if (!error) return;
 
   return (
     <div>
